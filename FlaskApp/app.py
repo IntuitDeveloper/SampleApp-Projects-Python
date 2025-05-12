50ae8e8e011b86f538dca274c37c19d9293a39cd
smohan11 <soumiya_mohan@intuit.com>
2025-05-12 13:30:35 -0700

    Move GraphQL mutation to separate file

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -129,38 +129,10 @@ def getProjects():
         )
         
         client = Client(transport=transport, fetch_schema_from_transport=False)
-        query = gql("""
-            mutation ProjectManagementCreateProject($name: String!, $description: String, $startDate: DateTime, $dueDate: DateTime!, $status: ProjectManagement_Status, $customer: ProjectManagement_CustomerInput, $priority: Int, $pinned: Boolean, $completionRate: Decimal, $emailAddress: [Qb_EmailAddressInput], $addresses: [Qb_PostalAddressInput]) {
-                projectManagementCreateProject(input:{
-                    name: $name,
-                    description: $description,
-                    startDate: $startDate,
-                    dueDate: $dueDate,
-                    status: $status,
-                    customer: $customer,
-                    priority: $priority,
-                    pinned: $pinned,
-                    completionRate: $completionRate,
-                    emailAddress: $emailAddress,
-                    addresses: $addresses
-                }) {
-                    ... on ProjectManagement_Project {
-                        id
-                        name
-                        description
-                        startDate
-                        dueDate
-                        status
-                        priority
-                        customer { id }
-                        pinned
-                        completionRate
-                        emailAddress { email name }
-                        addresses { streetAddressLine1 streetAddressLine2 streetAddressLine3 state postalCode }
-                    }
-                }
-            }
-        """)
+        
+        # Read the GraphQL mutation from file
+        with open('static/graphql/project.graphql', 'r') as f:
+            query = gql(f.read())
         
         now = datetime.datetime.now(datetime.timezone.utc)
         formatted_date = now.strftime('%Y-%m-%dT%H:%M:%S.') + f"{now.microsecond // 1000:03d}Z"
